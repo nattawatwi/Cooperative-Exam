b2a0ef748a5e9b25803e4eb58c0b03e2ec0dbdea
Nattawat Wilai <76519685+nattawatwi@users.noreply.github.com>
2023-06-23 12:34:07 +0700

    create

--- index.php
+++ index.php
@@ -15,6 +15,21 @@ include "db_conn.php";
 
   <!-- Font Awesome -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  
+  <style>
+    a.link {
+      color: red; 
+    }
+    i.fa-eye {
+      color: lightgreen; 
+    }
+    i.fa-pen-to-square {
+      color: black; 
+    }
+    i.fa-trash {
+      color: red
+    }
+  </style>
 
   <title> SWOT Analysis </title>
 </head>
@@ -40,11 +55,11 @@ include "db_conn.php";
     </div>';
     }
     ?>
-    <input type="text" id="myInput" onkeyup="myFunction()" placeholder="ค้นหา ..." title="Type in a name">
+    <input type="text" id="myInput" onkeyup="myFunction()" placeholder="Search for names.." title="Type in a name">
     <a href="add-new.php" class="btn btn-dark mb-3 float-end">เพิ่ม</a>
 
     <table id="myTable" class="table table-hover text-center">
-      <thead class="table-dark">
+      <thead class="table-light">
         <tr>
           <th scope="col">ID</th>
           <th scope="col">พื้นที่</th>
@@ -65,9 +80,9 @@ include "db_conn.php";
             <td><?php echo $row["name"] ?></td>
             <td><?php echo $row["date"] ?></td>
             <td>
-              <a href="detail.php?id=<?php echo $row["id"] ?>" class="link-dark"><i class="fa-solid fa-eye fs-5 me-1"></i></a>
-              <a href="edit.php?id=<?php echo $row["id"] ?>" class="link-dark"><i class="fa-solid fa-pen-to-square fs-5 me-1"></i></a>
-              <a href="delete.php?id=<?php echo $row["id"] ?>" class="link-dark"><i class="fa-solid fa-trash fs-5"></i></a>
+              <a href="detail.php?id=<?php echo $row["id"] ?>" class="link"><i class="fa-solid fa-eye fs-5 me-1"></i></a>
+              <a href="edit.php?id=<?php echo $row["id"] ?>" class="link"><i class="fa-solid fa-pen-to-square fs-5 me-1"></i></a>
+              <a href="delete.php?id=<?php echo $row["id"] ?>" class="link"><i class="fa-solid fa-trash fs-5"></i></a>
             </td>
           </tr>
         <?php
@@ -101,7 +116,7 @@ include "db_conn.php";
 
   <!-- Bootstrap -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-
+  
 </body>
 
 </html>
